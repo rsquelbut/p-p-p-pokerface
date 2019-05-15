3566a6bf8e19289902557e19749766f7dbd95534
Raphaël Squelbut <rsquelbut@electre.com>
2019-05-15 18:49:50 +0200

    fix leiningein conf

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject p-p-p-pokerface "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [iloveponies.tests/p-p-p-pokerface "0.1.0-SNAPSHOT"]]
-  :profiles {:dev {:plugins [[lein-midje "3.1.1"]]}})
+(defproject structured-data "1.0.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [iloveponies.tests/structured-data "0.1.0-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[midje "1.9.5" :exclusions [org.clojure/clojure]]]
+                   :plugins [[lein-midje "3.2.1"]]}})
